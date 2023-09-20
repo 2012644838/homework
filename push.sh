b93e12e45ee5c36daf0a509780ecfbe75def6ec4
2012644838 <2012644838@qq.com>
2023-09-20 10:26:18 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 git config --global user.name '2012644838'
 git config --global user.email '2012644838@qq.com'
 git config --global http.sslverify false
